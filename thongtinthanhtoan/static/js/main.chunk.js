c15542d355be4ded10be25dd4922b8965f73efb2
alextranminhhun <102737428+alextranminhhun@users.noreply.github.com>
2022-09-02 15:36:02 +0700

    â
    a

--- thongtinthanhtoan/static/js/main.chunk.js
+++ thongtinthanhtoan/static/js/main.chunk.js
@@ -117,7 +117,7 @@
                                                 logo: "mm",
                                                 name: "Ví MoMo",
                                                 ctk: "Lê Hồng Anh Tùng ",
-                                                stk: "0977759963"
+                                                stk: "0961869322"
                                             }, {
                                                 logo: "tsr",
                                                 name: "TheSieuRe",
@@ -206,7 +206,7 @@
                                                 children: [Object(d.jsx)("span", {
                                                     className: "overhead",
                                                     children: "Email"
-                                                }), "kazuto@lehonganhtung.tk "]
+                                                }), "lehonganhtung. kazuto@gmail.com"]
                                             }), Object(d.jsxs)("li", {
                                                 children: [Object(d.jsx)("span", {
                                                     className: "overhead",
